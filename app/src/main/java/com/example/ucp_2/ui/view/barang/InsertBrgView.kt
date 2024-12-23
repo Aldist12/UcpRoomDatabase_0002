20bd8deca9f7a6abca9b736a2f2e13ccb3bb5dcf
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-23 22:49:55 +0700

    Menambahkan fungsi komponen body form input barang

--- app/src/main/java/com/example/ucp_2/ui/view/barang/InsertBrgView.kt
+++ app/src/main/java/com/example/ucp_2/ui/view/barang/InsertBrgView.kt
@@ -1,5 +1,34 @@
 package com.example.ucp_2.ui.view.barang
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp_2.R
+import com.example.ucp_2.ui.costumwidget.TopAppBar
+import kotlinx.coroutines.launch
+
 @Composable
 fun InsertBrgView(
     onBackArrow: () -> Unit,
@@ -50,6 +79,61 @@ fun InsertBrgView(
                         onNavigate()
                     }
                 }
+            )
+        }
+    }
+}
+
+@Composable
+fun InsertBodyBrg(
+    modifier: Modifier = Modifier,
+    formTitle: String,
+    onValueChange: (BarangEvent) -> Unit,
+    uiState: brgUIState,
+    onClick: suspend () -> Unit,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .background(MaterialTheme.colorScheme.surface, MaterialTheme.shapes.medium),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = formTitle,
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.align(Alignment.Start)
+        )
+        FormBarang(
+            barangEvent = uiState.barangEvent,
+            onValueChange = onValueChange,
+            errorBrgState = uiState.isEntryBrgValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    onClick()
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            shape = MaterialTheme.shapes.medium,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            )
+        ) {
+            Text(
+                text = "Simpan",
+                color = Color.White,
+                fontSize = 18.sp
             )
         }
     }
