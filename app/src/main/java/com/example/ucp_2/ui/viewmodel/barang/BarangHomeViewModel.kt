d73e4c84d84e6f7f5836f186779f92d803b8cde2
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-24 00:16:27 +0700

    Menambahkan proses state alur data barang

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/BarangHomeViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/BarangHomeViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.ucp_2.ui.viewmodel.barang
 
-class BarangHomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.ucp_2.repository.RepositoryBrg
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+
+class BarangHomeViewModel(
+    private val repositoryBrg: RepositoryBrg
+) : ViewModel() {
+
+    val homeUiStateBrg: StateFlow<HomeUIStateBrg> = repositoryBrg.getAllBrg()
+        .filterNotNull()
+        .map {
+            HomeUIStateBrg(
+                listBarang = it.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(HomeUIStateBrg(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            HomeUIStateBrg(
+                isLoading = false,
+                isError = true,
+                errorMessage = it.message ?: "Terjadi Kesalahan"
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUIStateBrg(
+                isLoading = true
+            )
+        )
 }
