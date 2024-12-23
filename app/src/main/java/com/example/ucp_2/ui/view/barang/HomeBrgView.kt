891d42f1c5928122f5b966aa23fb69221118535c
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-23 22:27:10 +0700

    Tambahkan tampilan utama untuk Data Barang dengan TopAppBar dan FAB

---
+++ app/src/main/java/com/example/ucp_2/ui/view/barang/HomeBrgView.kt
@@ -0,0 +1,55 @@
+package com.example.ucp_2.ui.view.barang
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp_2.R
+import com.example.ucp_2.ui.costumwidget.TopAppBar
+
+@Composable
+fun HomeBrgView(
+    viewModel: BarangHomeViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddBrgClick: () -> Unit = { },
+    onDetailBrgClick: (String) -> Unit = { },
+    onBackArrow: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = "Data Barang",
+                onBackClick = onBackArrow,
+                actionIcon = R.drawable.box
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddBrgClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Barang"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeBrgUiState by viewModel.homeUiStateBrg.collectAsState()
+
+        BodyHomeBrgView(
+            homeUiState = homeBrgUiState,
+            onClick = {
+                onDetailBrgClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
