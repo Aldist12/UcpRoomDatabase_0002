857f7260da441b048169aa2a63440c5a7f939045
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-23 23:57:19 +0700

    Menambahkan fungsi komponen header beranda dengan welcome message

--- app/src/main/java/com/example/ucp_2/ui/view/Homepage.kt
+++ app/src/main/java/com/example/ucp_2/ui/view/Homepage.kt
@@ -1,12 +1,34 @@
 package com.example.ucp_2.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.R
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true)
 @Composable
@@ -21,5 +43,61 @@ fun Homepage(
     ) {
         HeaderSection()
         BodySection(onItemClick = onItemClick)
+    }
+}
+
+@Composable
+fun HeaderSection() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(bottomEnd = 48.dp))
+            .background(color = colorResource(R.color.primary))
+            .padding(bottom = 32.dp)
+            .padding(top = 20.dp)
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, top = 24.dp)
+        ) {
+            Column {
+                Icon(
+                    Icons.Filled.Home,
+                    contentDescription = "Home Icon",
+                    tint = Color.White,
+                    modifier = Modifier.padding(8.dp)
+                )
+                Spacer(Modifier.padding(3.dp))
+                Text(
+                    text = "Discover the Power Behind the Products",
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                Text(
+                    text = "Welcome to Our Warehouse!",
+                    fontSize = 22.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
+        }
+        Box(
+            Modifier.align(Alignment.CenterEnd)
+                .padding(24.dp)
+                .padding(top = 12.dp)
+        )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.warehouse),
+                contentDescription = "Photo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            )
+        }
     }
 }
