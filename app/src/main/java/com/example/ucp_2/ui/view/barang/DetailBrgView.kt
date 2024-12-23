60562adcef9fbc4e3284f53e414d87dbe946c6f3
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-23 23:23:16 +0700

    Menambahkan fungsi komponen untuk menampilkan detail informasi barang

--- app/src/main/java/com/example/ucp_2/ui/view/barang/DetailBrgView.kt
+++ app/src/main/java/com/example/ucp_2/ui/view/barang/DetailBrgView.kt
@@ -1,6 +1,7 @@
 package com.example.ucp_2.ui.view.barang
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,7 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -23,13 +25,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp_2.R
 import com.example.ucp_2.data.entity.Barang
@@ -276,5 +281,33 @@ private fun DeleteConfirmationDialog(
             }
         }
     )
+}
+@Composable
+fun ComponentDetailBrg(
+    modifier: Modifier = androidx.compose.ui.Modifier,
+    title: String,
+    content: String,
+    iconContent: @Composable () -> Unit
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        iconContent()
+        Spacer(modifier = androidx.compose.ui.Modifier.width(8.dp))
+        Column {
+            Text(
+                text = "$title:",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
+            Text(
+                text = content,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal
+            )
+        }
+    }
 }
 
