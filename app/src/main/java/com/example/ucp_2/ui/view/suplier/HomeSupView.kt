f6602d302050324d9bcab3bdd24b42f69c797ba8
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-23 23:31:58 +0700

    Menambahkan fungsi body tampilan utama supplier dengan penanganan state

--- app/src/main/java/com/example/ucp_2/ui/view/suplier/HomeSupView.kt
+++ app/src/main/java/com/example/ucp_2/ui/view/suplier/HomeSupView.kt
@@ -1,5 +1,7 @@
 package com.example.ucp_2.ui.view.suplier
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -8,11 +10,20 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.R
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp_2.ui.costumwidget.TopAppBar
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeSupView(
@@ -48,5 +59,48 @@ fun HomeSupView(
             homeUiState = homeSplUiState,
             modifier = modifier.padding(innerPadding)
         )
+    }
+}
+@Composable
+fun BodyHomeSupView(
+    homeUiState: HomeUIStateSpl,
+    modifier: Modifier = Modifier,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    when {
+        homeUiState.isLoading -> {
+            LoadingState()
+        }
+
+        homeUiState.isError -> {
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        homeUiState.listSpl.isEmpty() -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data Supplier.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            ListSupplier(
+                listSup = homeUiState.listSpl,
+                modifier = modifier
+            )
+        }
     }
 }
