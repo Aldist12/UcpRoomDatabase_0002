fd18fb3124ea21a74de7854ceb9334655b55a733
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-25 04:04:31 +0700

    Menyediakan state management dengan `StateFlow` untuk loading, error, dan data sukses

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/suplier/SupplierHomeViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/suplier/SupplierHomeViewModel.kt
@@ -1,8 +1,40 @@
 package com.example.ucp_2.ui.viewmodel.suplier
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp_2.repository.RepositorySup
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
 class SupplierHomeViewModel(
     private val repositorySpl: RepositorySup
 ) : ViewModel() {
+    val homeUiStateSpl: StateFlow<HomeUIStateSpl> = repositorySpl.getAllSupplier()
+        .filterNotNull()
+        .map {
+            HomeUIStateSpl(
+                listSpl = it.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(HomeUIStateSpl(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUIStateSpl(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUIStateSpl(
+                isLoading = true
+            )
+        )
+}
