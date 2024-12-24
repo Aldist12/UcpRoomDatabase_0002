c6ca74cc78120475555b4a90bdccf262c9b9e631
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-25 04:09:43 +0700

    Menambahkan fungsi `validateSplFields` untuk validasi data input supplier

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/suplier/SupplierViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/suplier/SupplierViewModel.kt
@@ -10,3 +10,14 @@ class SupplierViewModel(private val repositorySpl: RepositorySup) : ViewModel() 
     fun updateSplState(supplierEvent: SupplierEvent) {
         uiSplState = uiSplState.copy(supplierEvent = supplierEvent)
     }
+    private fun validateSplFields(): Boolean {
+        val event = uiSplState.supplierEvent
+        val errorSplState = FormErrorSplState(
+            nama = if (event.nama.isNotEmpty()) null else "Nama Tidak Boleh Kosong",
+            kontak = if (event.kontak.isNotEmpty()) null else "Kontak Tidak Boleh Kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat Tidak Boleh Kosong",
+        )
+
+        uiSplState = uiSplState.copy(isEntrySplValid = errorSplState)
+        return errorSplState.isSplValid()
+    }
