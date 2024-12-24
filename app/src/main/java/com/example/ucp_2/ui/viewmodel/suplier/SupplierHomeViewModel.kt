ae2eea2b8e18120b0ba476282b4628ff6ad1ec83
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-12-25 04:02:06 +0700

    Menambahkan class`SupplierHomeViewModel` untuk mengelola data daftar supplier

---
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/suplier/SupplierHomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.ucp_2.ui.viewmodel.suplier
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp_2.repository.RepositorySup
+
+class SupplierHomeViewModel(
+    private val repositorySpl: RepositorySup
+) : ViewModel() {
